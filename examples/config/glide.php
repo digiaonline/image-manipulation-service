917d3e8142be12c9b3b34ec89b066e7a8bd31ad1
Sam Stenvall <sam.stenvall@nordsoftware.com>
2017-04-03 14:15:42 +0300

    Add an example on how to specify default parameters

--- examples/config/glide.php
+++ examples/config/glide.php
@@ -2,11 +2,17 @@
 
 return [
     // Source filesystem
-    'source'   => new League\Flysystem\Filesystem(new League\Flysystem\Adapter\Local(storage_path('files'))),
+    'source'         => new League\Flysystem\Filesystem(new League\Flysystem\Adapter\Local(storage_path('files'))),
     // Cache filesystem
-    'cache'    => new League\Flysystem\Filesystem(new League\Flysystem\Memory\MemoryAdapter()),
+    'cache'          => new League\Flysystem\Filesystem(new League\Flysystem\Memory\MemoryAdapter()),
     // Image driver (gd or imagick)
-    'driver'   => 'gd',
+    'driver'         => 'gd',
     // Response factory
-    'response' => new League\Glide\Responses\LaravelResponseFactory(),
+    'response'       => new League\Glide\Responses\LaravelResponseFactory(),
+    // Max image size
+    'max_image_size' => 2000 * 2000,
+    // Defaults (uncomment and modify if necessary)
+    //'defaults'       => [
+    //    'fm' => 'jpg',
+    //],
 ];
