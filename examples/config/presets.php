04b3b1e8d2a23ec8a36c545b3fb1780b26ccd026
Sam Stenvall <sam.stenvall@nordsoftware.com>
2017-04-03 14:16:19 +0300

    Add documentation on what presets.php should look like and what it's for

--- examples/config/presets.php
+++ examples/config/presets.php
@@ -1,5 +1,11 @@
 <?php
 
+/**
+ * Defines the presets that can be used as shortcuts when requesting manipulated images. Each key in the array is the
+ * preset name while the value is the corresponding options for Glide.
+ *
+ * If you don't want to use presets at all, return an empty array.
+ */
 return [
     'article-small' => [
         'w' => 400,
