a254825add25dcd7ad970a904137d136001ac066
Sam Stenvall <sam.stenvall@nordsoftware.com>
2017-09-06 14:30:12 +0300

    Use 404 when a file was not found (closes GitHub #23)

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,11 +2,14 @@
 
 namespace Nord\ImageManipulationService\Exceptions;
 
+use Exception;
 use Illuminate\Auth\Access\AuthorizationException;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Validation\ValidationException;
 use Laravel\Lumen\Exceptions\Handler as ExceptionHandler;
+use League\Flysystem\FileNotFoundException;
 use Symfony\Component\HttpKernel\Exception\HttpException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 /**
  * Class Handler
@@ -26,5 +29,18 @@ class Handler extends ExceptionHandler
         ModelNotFoundException::class,
         ValidationException::class,
     ];
+
+    /**
+     * @inheritdoc
+     */
+    public function render($request, Exception $e)
+    {
+        // Change FileNotFoundException to NotFoundHttpException so we get the correct HTTP status code 
+        if ($e instanceof FileNotFoundException) {
+            $e = new NotFoundHttpException($e->getMessage(), $e);
+        }
+
+        return parent::render($request, $e);
+    }
 
 }
